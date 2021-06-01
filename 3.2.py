c35deb15dc8e6418500f6024bcb1d3fd8ccb9112
Hronnos <66788792+Hronnos@users.noreply.github.com>
2021-06-01 18:49:33 +0300

    Update 3.2.py

--- 3.2.py
+++ 3.2.py
@@ -1,12 +1,8 @@
 symbols_list = ['a', 'b', 'c', 'c']
 
 def first_rep(s):
-    try:
-        for i in range(len(s) - 1):
-            assert s[i] == str(s[i])
-            if s[i] == s[i + 1]:
-                return s[i]
-    except AssertionError:
-           return 'Это не строка!'
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return s[i]
 
 print(first_rep(symbols_list))
